c8752dd344001921fb785091b03d9494cb1da12a
Mykolas Cibas <mykolas.cibas@saldo.com>
2025-01-31 12:03:43 +0200

    setup

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+
+import cloudflare from '@astrojs/cloudflare'
+
+// https://astro.build/config
+export default defineConfig({
+  site: process.env.CI
+    ? 'https://ssn-frontend.pages.dev'
+    : 'http://localhost:4321',
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+
+  output: 'server',
+  adapter: cloudflare(),
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        'react-dom/server': 'react-dom/server.edge',
+      },
+    },
+  },
+})
